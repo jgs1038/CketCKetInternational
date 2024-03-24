197f557921d7638c328e771362397979970e797e
lydia <lbr1014@alu.ubu.es>
2024-03-24 18:44:42 +0100

    Perfil evento

---
+++ app/src/main/java/com/MBLJ/cketcket/PerfilEvento.java
@@ -0,0 +1,22 @@
+package com.MBLJ.cketcket;
+
+//Esto es una prueba
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilEvento extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_evento);
+
+    }
+
+
+
+
+}
