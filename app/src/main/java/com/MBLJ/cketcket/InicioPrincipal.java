1f9edb2ba1ed60814769d19313bad00b363fc816
lydia <lbr1014@alu.ubu.es>
2024-03-26 14:23:41 +0100

    fallo arreglado

--- app/src/main/java/com/MBLJ/cketcket/InicioPrincipal.java
+++ app/src/main/java/com/MBLJ/cketcket/InicioPrincipal.java
@@ -9,16 +9,14 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InicioPrincipal extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_inicio);
-
     }
-
     public void SiguienteNicki(View view){
-        Intent siguiente = new Intent (this, PerfilEvento.class);
-        startActivity(siguiente);
+        Intent siguienteNicki = new Intent (this, PerfilEvento.class);
+        startActivity(siguienteNicki);
     }
-
 }
