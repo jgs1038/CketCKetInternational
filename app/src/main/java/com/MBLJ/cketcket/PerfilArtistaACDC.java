d996d7670fb797240d0bf4ad50f18b8a007d2954
lydia <lbr1014@alu.ubu.es>
2024-04-01 18:40:44 +0200

    más pantallas

--- app/src/main/java/com/MBLJ/cketcket/PerfilArtistaACDC.java
+++ app/src/main/java/com/MBLJ/cketcket/PerfilArtistaACDC.java
@@ -12,7 +12,7 @@ public class PerfilArtistaACDC extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.perfil_artista_kiki);
+        setContentView(R.layout.perfil_artista_acdc);
 
     }
     public void SiguienteLupa(View view){
