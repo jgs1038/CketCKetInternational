97f806559a425902628e16db0840b6ea2b92e72a
lydia <lbr1014@alu.ubu.es>
2024-04-03 16:10:23 +0200

    cambios

--- app/src/main/java/com/MBLJ/cketcket/Chat.java
+++ app/src/main/java/com/MBLJ/cketcket/Chat.java
@@ -3,15 +3,21 @@ package com.MBLJ.cketcket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Chat extends AppCompatActivity  {
+    private EditText et2;
+    private TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat);
+        et2 = (EditText) findViewById(R.id.txt_num2);
+        tv1 = (TextView) findViewById(R.id.txt_resultado);
     }
     public void SiguienteLupa(View view){
         Intent siguiente = new Intent (this, BusquedaUsuario.class);
@@ -25,6 +31,11 @@ public class Chat extends AppCompatActivity  {
     public void SiguienteTresPuntods(View view) {
         Intent siguiente = new Intent(this, TresPuntos.class);
         startActivity(siguiente);
+    }
+    public void Escribir(View view){
+        String result = et2.getText().toString();
+        tv1.setText(result);
+
     }
 
 }
