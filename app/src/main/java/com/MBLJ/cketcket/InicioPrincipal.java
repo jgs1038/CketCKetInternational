44eab9aff9b7a1e63c928193e306ff172da1f3ca
JGS <jgs1038@alu.ubu.es>
2024-04-10 16:12:22 +0200

    El menú con scroll horizontal ¿funcionará?

--- app/src/main/java/com/MBLJ/cketcket/InicioPrincipal.java
+++ app/src/main/java/com/MBLJ/cketcket/InicioPrincipal.java
@@ -5,15 +5,34 @@ package com.MBLJ.cketcket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class InicioPrincipal extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private MyRvAdapter adapter;
+    private List<String> dataSource;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.principal_inicio);
+        setContentView(R.layout.principal_inicio); // Asegúrate de que este es el layout correcto
+
+        // Inicialización de la fuente de datos
+        dataSource = Arrays.asList("Hello", "World", "To", "The", "Code", "City", "******");
+
+        // Configuración del RecyclerView
+        recyclerView = findViewById(R.id.myRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        // Configuración del adaptador
+        adapter = new MyRvAdapter(this, dataSource);
+        recyclerView.setAdapter(adapter);
     }
 
     public void SiguienteLupa(View view){
