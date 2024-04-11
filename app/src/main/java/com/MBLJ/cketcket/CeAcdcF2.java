5f851ed153f39b6862bea8df7afef5f15578ab20
lydia <lbr1014@alu.ubu.es>
2024-04-11 14:21:00 +0200

    menus añadidos

--- app/src/main/java/com/MBLJ/cketcket/CeAcdcF2.java
+++ app/src/main/java/com/MBLJ/cketcket/CeAcdcF2.java
@@ -1,5 +1,6 @@
 package com.MBLJ.cketcket;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,47 +17,100 @@ public class CeAcdcF2 extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ceacdcf2);
     }
+    public int obtenerTipoUsuario() {
+        SharedPreferences prefs = getSharedPreferences("PreferenciasUsuario", MODE_PRIVATE);
+        return prefs.getInt("tipoUsuario", 0);
+    }
+
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.tres_rayas, menu);
+        int tipoUsuario=obtenerTipoUsuario();
+
+        if (tipoUsuario == 1) {
+            menuInflater.inflate(R.menu.tres_rayas, menu);
+        }
+        if (tipoUsuario == 2) {
+            menuInflater.inflate(R.menu.tres_rayas_artista, menu);
+        }
+        if (tipoUsuario == 3) {
+            menuInflater.inflate(R.menu.tres_rayas_organizador, menu);
+        }
         return true;
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         int id = item.getItemId();
-        if (id == R.id.mis_entradas){
-            Intent siguiente = new Intent (this, MisEntradas.class);
-            startActivity(siguiente);
+        int tipoUsuario=obtenerTipoUsuario();
+        if (tipoUsuario==1) {
+            if (id == R.id.mis_entradas) {
+                Intent siguiente = new Intent(this, MisEntradas.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.ForoDudas) {
+                Intent siguiente = new Intent(this, ForoDudas.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.asistencia_tecnica) {
+                Intent siguiente = new Intent(this, AsistenciaTecnica.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.chat) {
+                Intent siguiente = new Intent(this, Chat1.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.mis_puntos) {
+                Intent siguiente = new Intent(this, MisPuntos.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.usuarios_bloqueados) {
+                Intent siguiente = new Intent(this, UsuariosBloqueados.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.live_activities) {
+                Intent siguiente = new Intent(this, LiveActivities.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.merchandising) {
+                Intent siguiente = new Intent(this, Merchandising.class);
+                startActivity(siguiente);
+            }
         }
-        if (id == R.id.ForoDudas){
-            Intent siguiente = new Intent (this, ForoDudas.class);
-            startActivity(siguiente);
+        if (tipoUsuario==2){
+            if (id == R.id.solicitud_evento){
+                Intent siguiente = new Intent (this, SolicitudEventoArtista.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.datos_evento){
+                Intent siguiente = new Intent (this, ModificarDatosEvento.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.directo){
+                Intent siguiente = new Intent (this, EmpezarDirecto.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.encuesta){
+                Intent siguiente = new Intent (this, CrearEncuesta.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.preguntas_respuestas){
+                Intent siguiente = new Intent (this, PreguntasRespuestas.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.historia){
+                Intent siguiente = new Intent (this, AnadirHistoria.class);
+                startActivity(siguiente);
+            }
         }
-        if (id == R.id.asistencia_tecnica){
-            Intent siguiente = new Intent (this, AsistenciaTecnica.class);
-            startActivity(siguiente);
+        if (tipoUsuario==3) {
+            if (id == R.id.publicar_evento) {
+                Intent siguiente = new Intent(this, SolicitudEventoOrganizador.class);
+                startActivity(siguiente);
+            }
+            if (id == R.id.modificar_evento) {
+                Intent siguiente = new Intent(this, ModificarDatosEvento.class);
+                startActivity(siguiente);
+            }
         }
-        if (id == R.id.chat){
-            Intent siguiente = new Intent (this, Chat1.class);
-            startActivity(siguiente);
-        }
-        if (id == R.id.mis_puntos){
-            Intent siguiente = new Intent (this, MisPuntos.class);
-            startActivity(siguiente);
-        }
-        if (id == R.id.usuarios_bloqueados){
-            Intent siguiente = new Intent (this, UsuariosBloqueados.class);
-            startActivity(siguiente);
-        }
-        if (id == R.id.live_activities){
-            Intent siguiente = new Intent (this, LiveActivities.class);
-            startActivity(siguiente);
-        }
-        if (id == R.id.merchandising){
-            Intent siguiente = new Intent (this, Merchandising.class);
-            startActivity(siguiente);
-        }
-
         //Toast.makeText(this,"Fallo", Toast.LENGTH_SHORT).show();
         //return super.onOptionsItemSelected(item);
         return true;
