fa08d8838fd20e1c5b434fb6176b8fa55339767b
Marcurio222 <mzl1004@alu.ubu.es>
2024-04-14 21:22:01 +0200

    Pasarela del coletas Faltan cosas todavía

--- app/src/main/java/com/MBLJ/cketcket/PasarelaPagoACDCTarjeta.java
+++ app/src/main/java/com/MBLJ/cketcket/PasarelaPagoACDCTarjeta.java
@@ -5,90 +5,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.CheckBox;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 //imports del menu
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import android.view.Menu;
+import android.widget.Toast;
 
-
-public class PasarelaPagoACDC extends AppCompatActivity {
-
-    CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
-    Button button99;
+public class PasarelaPagoACDCTarjeta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pasarela_pago_acdc);
-
-        checkBox1 = findViewById(R.id.checkBox1);
-        checkBox2 = findViewById(R.id.checkBox2);
-        checkBox3 = findViewById(R.id.checkBox3);
-        checkBox4 = findViewById(R.id.checkBox4);
-        button99 = findViewById(R.id.button99);
-
-        checkBox1.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                if (isChecked) {
-                    checkBox2.setChecked(false);
-                    checkBox3.setChecked(false);
-                    checkBox4.setChecked(false);
-                    button99.setEnabled(true);
-                    button99.setOnClickListener(v -> {
-                        Intent intent = new Intent(PasarelaPagoACDC.this, PasarelaPagoACDCTarjeta.class);
-                        startActivity(intent);
-                    });
-                } else {
-                    button99.setEnabled(false);
-                }
-            });
-
-        checkBox2.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                if (isChecked) {
-                    checkBox1.setChecked(false);
-                    checkBox3.setChecked(false);
-                    checkBox4.setChecked(false);
-                    button99.setEnabled(true);
-                    button99.setOnClickListener(v -> {
-                        Intent intent = new Intent(PasarelaPagoACDC.this, PasarelaPagoACDCContrareembolso.class);
-                        startActivity(intent);
-                    });
-                } else {
-                    button99.setEnabled(false);
-                }
-            });
-
-        checkBox3.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                checkBox1.setChecked(false);
-                checkBox2.setChecked(false);
-                checkBox4.setChecked(false);
-                button99.setEnabled(true);
-                button99.setOnClickListener(v -> {
-                    Intent intent = new Intent(PasarelaPagoACDC.this, PasarelaPagoACDCPayPal.class);
-                    startActivity(intent);
-                });
-            } else {
-                button99.setEnabled(false);
-            }
-        });
-        checkBox4.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                checkBox1.setChecked(false);
-                checkBox2.setChecked(false);
-                checkBox3.setChecked(false);
-                button99.setEnabled(true);
-                button99.setOnClickListener(v -> {
-                    Intent intent = new Intent(PasarelaPagoACDC.this, PasarelaPagoACDCRegalo.class);
-                    startActivity(intent);
-                });
-            } else {
-                button99.setEnabled(false);
-            }
-        });
+        setContentView(R.layout.pasarela_pago_acdc_tarjeta);
     }
 
 
@@ -97,9 +31,9 @@ public class PasarelaPagoACDC extends AppCompatActivity {
         return prefs.getInt("tipoUsuario", 0);
     }
 
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        int tipoUsuario=obtenerTipoUsuario();
+        int tipoUsuario = obtenerTipoUsuario();
 
         if (tipoUsuario == 1) {
             menuInflater.inflate(R.menu.tres_rayas, menu);
@@ -115,11 +49,12 @@ public class PasarelaPagoACDC extends AppCompatActivity {
         }
         return true;
     }
+
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        int tipoUsuario=obtenerTipoUsuario();
-        if (tipoUsuario==1) {
+        int tipoUsuario = obtenerTipoUsuario();
+        if (tipoUsuario == 1) {
             if (id == R.id.mis_entradas) {
                 Intent siguiente = new Intent(this, MisEntradas.class);
                 startActivity(siguiente);
@@ -153,7 +88,7 @@ public class PasarelaPagoACDC extends AppCompatActivity {
                 startActivity(siguiente);
             }
         }
-        if (tipoUsuario==2) {
+        if (tipoUsuario == 2) {
             if (id == R.id.mis_entradas) {
                 Intent siguiente = new Intent(this, MisEntradas.class);
                 startActivity(siguiente);
@@ -186,17 +121,17 @@ public class PasarelaPagoACDC extends AppCompatActivity {
                 Intent siguiente = new Intent(this, Merchandising.class);
                 startActivity(siguiente);
             }
-            if (id == R.id.solicitud_evento){
-                Intent siguiente = new Intent (this, SolicitudEventoArtista.class);
+            if (id == R.id.solicitud_evento) {
+                Intent siguiente = new Intent(this, SolicitudEventoArtista.class);
                 startActivity(siguiente);
             }
-            if (id == R.id.datos_evento){
-                Intent siguiente = new Intent (this, ModificarDatosEvento.class);
+            if (id == R.id.datos_evento) {
+                Intent siguiente = new Intent(this, ModificarDatosEvento.class);
                 startActivity(siguiente);
             }
         }
 
-        if (tipoUsuario==3) {
+        if (tipoUsuario == 3) {
             if (id == R.id.mis_entradas) {
                 Intent siguiente = new Intent(this, MisEntradas.class);
                 startActivity(siguiente);
@@ -239,7 +174,7 @@ public class PasarelaPagoACDC extends AppCompatActivity {
             }
         }
 
-        if (tipoUsuario==4) {
+        if (tipoUsuario == 4) {
             if (id == R.id.mis_entradas) {
                 Intent siguiente = new Intent(this, MisEntradas.class);
                 startActivity(siguiente);
@@ -293,5 +228,9 @@ public class PasarelaPagoACDC extends AppCompatActivity {
         //Toast.makeText(this,"Fallo", Toast.LENGTH_SHORT).show();
         //return super.onOptionsItemSelected(item);
         return true;
+    }
+    public void SiguientePorPrecios(View view) {
+        Intent siguiente = new Intent(this, CeAcdcF2.class);
+        startActivity(siguiente);
     }
 }
