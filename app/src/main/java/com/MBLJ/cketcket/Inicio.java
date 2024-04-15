b774e66564764724af1efae6acedc1a7b8a96fd8
BLG <blg1006@alu.ubu.es>
2024-04-15 17:15:40 +0200

    Para que salga lo del mensajito de iniciando al principio

--- app/src/main/java/com/MBLJ/cketcket/Inicio.java
+++ app/src/main/java/com/MBLJ/cketcket/Inicio.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class Inicio extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
+        Toast.makeText(this, "Iniciando", Toast.LENGTH_SHORT).show();
     }
 
     public void SiguienteRegistro(View view) {
